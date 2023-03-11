61b7cf188ddfc17fba9c10b70a867d1ced7230f2
Carlos <carlos.inf.tech@gmail.com>
2023-03-11 17:07:59 +0100

    WIP Obtain dataframe ready to be fed into database

--- src/DVF/read_dvf_data.py
+++ src/DVF/read_dvf_data.py
@@ -1,44 +1,24 @@
 import pandas as pd
-from geopy.geocoders import Nominatim
 
-dvf_df = pd.read_csv("output.txt", sep="|")
-print(dvf_df.columns)
-
-#select values of interest, mainly date of transaction type of transaction
-
-print(dvf_df[['Date mutation',
-              'Nature mutation',
-              'Valeur fonciere',
-              ]].head(3))
-
-print(dvf_df[['Code departement',
-              'Code commune',
-              'Prefixe de section',
-              ]].head(3))
-
-print(dvf_df[[
-              'Section',
-              'No plan']].head(3))
-
-print(dvf_df[[
-              'No voie','Type de voie','Voie']].head(3))
-
-print(dvf_df[[
-              'Code postal','Commune']].head(3))
-
-#geolocator = Nominatim(user_agent = "carlos_python_geo_processor")
-#location = geolocator.geocode("16 rue des rigoles 75020 Paris France")
-#print(location.latitude,location.longitude)
-
-dvf_sample3 = dvf_df.head(10)
+from src.pipeline.transformer import Transformer
 
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 2000)
+pd.set_option('display.float_format', '{:20,.2f}'.format)
+pd.set_option('display.max_colwidth', None)
 
-def str_join(df, sep, *cols):
-   from functools import reduce
-   return reduce(lambda x, y: x.astype(str).str.cat(y.astype(str), sep=sep),
-                      [df[col] for col in cols])
+def transformDVFData():
+    dvf_df = pd.read_csv("output.txt", sep="|",nrows =10,dtype=object)
+    dvf_transformer = Transformer(dvf_df)
+    final_df = dvf_transformer.add_literal_to_df("country","France")\
+        .concatenate_columns("full_adress"," ",['No voie','Type de voie','Voie','Code postal','Commune','country'])\
+        .drop_na_in_columns(['No voie','Type de voie'])\
+        .get_longitude_and_latitude_from_adress("full_adress","latitude","longitude")\
+        .select_columns(["full_adress","latitude","longitude","Date mutation","Nature mutation","Valeur fonciere"])\
+        .data
+    print(final_df)
 
-# dvf_sample3['adress'] = dvf_sample3['No voie'].str.cat(dvf_sample3[['Type de voie','Comunne','Departement','Code postal']], sep=' ')
-dvf_sample3['adress'] = str_join(dvf_sample3,' ' ,'No voie', 'Type de voie', 'Commune', 'Code departement', 'Code postal')
-print(dvf_sample3['adress'])
+
+transformDVFData()
--- src/pipeline/transformer.py
+++ src/pipeline/transformer.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from datetime import datetime
+from geopy.geocoders import Nominatim
+from geopy.location import Location
 import uuid
 
 class Transformer:
 
     def __init__(self,data:pd.DataFrame):
+        self.geolocator = Nominatim(user_agent="carlos_python_geo_processor")
         self.data = data
 
     def cast_to_date(self,column_name:str,datetime_format:str):
@@ -15,11 +18,52 @@ class Transformer:
         self.data[column_name] = literal
         return self
 
-    def select_columns(self,column_name:str,literal:str):
-        self.data[column_name] = literal
+    def select_columns(self,columns:list[str]):
+        self.data = self.data[columns]
         return self
 
     def create_column_from_index(self,column_name:str):
         self.data[column_name] = self.data.index
         return self
 
+    def create_timestamp_column(self,column_name:str):
+        self.data[column_name] = self.data.index
+        return self
+
+    def concatenate_columns(self, column_name:str, separator:str, cols_to_concat:list[str]):
+        temp_df = self.data[cols_to_concat]
+        temp_df[cols_to_concat] = temp_df[cols_to_concat].astype(str)
+        temp_df[column_name] = temp_df[cols_to_concat].apply(separator.join,axis = 1)
+        self.data = self.data.join(temp_df[column_name])
+        return self
+
+    def rename_column(self,old_column:str, new_column:str):
+        self.data= self.data.rename({old_column:new_column},axis = 'columns')
+
+    def generate_uuid_column(self,column_name:str):
+        self.data[column_name] = [str(uuid.uuid4()) for _ in range(len(self.data.index))]
+
+    def cast_date_to_string(self,column_name:str,expected_output_format:str):
+        self.data[column_name] = self.data[column_name].dt.strftime(expected_output_format)
+        return self
+
+    def drop_na_in_columns(self,columns:list[str]):
+        self.data = self.data[self.data[columns].notnull().all(1)]
+        return self
+
+    def __get_location_of_address(self,adress:str):
+        location = self.geolocator.geocode(adress)
+        return location
+
+    def __get_latitude(self,geoloc:Location):
+        return geoloc.latitude
+
+    def __get_longitude(self,geoloc:Location):
+        return geoloc.longitude
+
+    def get_longitude_and_latitude_from_adress(self,adress_column:str,latitude_column:str,longitude_column:str):
+        self.data['geoloc'] = self.data[adress_column].apply(lambda x:self.__get_location_of_address(x))
+        self.data[latitude_column] = self.data['geoloc'].apply(lambda x:self.__get_latitude(x))
+        self.data[longitude_column] = self.data['geoloc'].apply(lambda x: self.__get_longitude(x))
+        self.data = self.data.drop('geoloc', axis=1)
+        return self
